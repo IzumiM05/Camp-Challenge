efd4c969822213476a7c4f490f8027e85d274177
IzumiM05 <weltreise00@gmail.com>
2016-10-03 01:41:32 +0900

    条件分岐

---
+++ 1_6Challenge.php
@@ -0,0 +1,17 @@
+<?php
+  $num=;
+  echo $num;
+
+  if ($num=1) {
+    echo "1です！";
+  }
+  elseif ($num=2) {
+    echo "プログラミングキャンプ！";
+  }
+  elseif ($num='a') {
+    echo "文字です！";
+  }
+  else {
+    echo "その他です！";
+  }
+?>
