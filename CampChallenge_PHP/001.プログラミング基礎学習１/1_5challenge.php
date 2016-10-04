07d59736cfb06ca9dbf92315983d31d33121f4b9
IzumiM05 <weltreise00@gmail.com>
2016-10-04 17:23:08 +0900

    1_1～5完成 1_6未完成

---
+++ CampChallenge_PHP/001.プログラミング基礎学習１/1_5challenge.php
@@ -0,0 +1,25 @@
+<?php
+  echo "1+2*3=";
+  echo (1+2*3)."<br>";
+
+  echo "-------"."<br>";
+
+  $tasu = 1 + 2 ;
+  $hiku = 3 - 1 ;
+  $kake = 2 * 4 ;
+  $wari = 6 / 3 ;
+
+  echo "$tasu - 2 = ".($tasu - 2)."<br>";
+  echo "$hiku + 4 = ".($hiku + 4)."<br>";
+  echo "$kake / 2 = ".($kake / 2)."<br>";
+  echo "$wari * 3 = ".($wari * 3)."<br>";
+
+  echo "-------"."<br>";
+
+  const A = 2 ;
+
+  echo "$tasu * A = ".($tasu * A)."<br>";
+  echo "$hiku + A = ".($hiku + A)."<br>";
+  echo "$kake / A = ".($kake / A)."<br>";
+  echo "$wari - A = ".($wari - A)."<br>";
+  ?>
---
+++ CampChallenge_PHP/001.プログラミング基礎学習１/1_6challenge.php
@@ -0,0 +1,18 @@
+<?php
+
+  $imput = "1" or "2" or "a"."<br>";
+
+  echo "1文字入力してください"."<br>";
+  
+
+  if ($imput = "1") {
+    echo "1です！"."<br>";
+  } else if ($imput = "2") {
+    echo "プログラミングキャンプ！"."<br>";
+  } else if ($imput = "a") {
+    echo "文字です！"."<br>";
+  } else {
+    echo "その他です！"."<br>";
+  }
+
+?>
